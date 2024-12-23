d474feb5bd8cbe354e35ff93f88f2e70f0143a7c
Amirhossein Emadi <emadi.henker404@gmail.com>
2024-12-23 13:13:36 +0330

    Add function to check for repeating numbers and update button action in index.html

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function isValidData() {
   const inputBox = document.querySelector("input[name=inputBox]");
   inputBox.value = inputBox.value.replace(/\D/g, "");
 }
+
+// checking the repeating number:
+function checkRepeatingNumber() {
+  const inputBox = document.querySelector("input[name=inputBox]").value;
+  // convert to the list:
+  const listOfNumbers = inputBox.split("").map((item) => Number(item));
+  console.log(listOfNumbers);
+}
