5b4e767ee0ee85c46a3dd06b363f0a9d8b70b8f6
Amirhossein Emadi <emadi.henker404@gmail.com>
2024-12-23 18:08:04 +0330

    Refactor number detection logic and update UI text for clarity in index.html and script.js

--- script.js
+++ script.js
@@ -9,8 +9,22 @@ function checkRepeatingNumber() {
   const inputBox = document.querySelector("input[name=inputBox]").value;
   // convert to the list:
   const listOfNumbers = inputBox.split("").map((item) => Number(item));
-  console.log(listOfNumbers);
-  alertOfResult(4);
+  // The unique numbers:
+  const uniqueNumbers = new Set(listOfNumbers);
+  const targetObject = [];
+  uniqueNumbers.forEach((value) => {
+    targetObject.push({
+      number: value,
+      repeat: listOfNumbers.filter((item) => item === value).length,
+    });
+  });
+  const listForMax = targetObject
+    .map((obj) => Object.values(obj)[1])
+    .filter((f) => f !== 1);
+  const foundItem = targetObject.find(
+    (el) => Object.values(el)[1] === Math.max(...listForMax)
+  );
+  alertOfResult(foundItem ? foundItem["number"] : "nothing!");
 }
 
 // show or hide alert of result:
